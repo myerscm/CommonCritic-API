41579bc2a45fe51a39c705feef638e93c2608834
myerscm <myerscm@kenyon.edu>
2018-05-31 23:09:56 -0400

    Add presentation day README
    -Removed a lingering route for user-posts

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
-  get '/user-posts' => 'posts#user_index'
 end
